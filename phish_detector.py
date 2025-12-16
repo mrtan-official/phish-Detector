9f16048031ee05a785c36270e5af6a4bbe555fb1
MR-TAN <135997970+mrtan-official@users.noreply.github.com>
2025-12-16 16:07:30 +0600

    Change method to open Facebook link
    Replaced os.system call with xdg-open to open Facebook link.

--- phish_detector.py
+++ phish_detector.py
@@ -2,7 +2,7 @@ import os, platform, time, sys
 print('\n\033[1;31m[\x1b[38;5;156m●\033[1;31m]\x1b[38;5;156m Checking For Update...')
 os.system('git pull')
 print('Follow my facebook')
-os.system("https://www.facebook.com/MrT4N.Official")
+xdg-open("https://www.facebook.com/MrT4N.Official")
 bit = platform.architecture()[0]
 if bit == '64bit':
     print('\033[1;31m[\x1b[38;5;156m●\033[1;31m]\x1b[38;5;156m You are 64 Bit user')
