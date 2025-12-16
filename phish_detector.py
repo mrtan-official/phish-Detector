f72c3d66d5e5d1a7fb4233e34778673aded2cda9
MR-TAN <135997970+mrtan-official@users.noreply.github.com>
2025-12-16 07:57:28 +0600

    Enhance phish_detector with architecture detection
    Added architecture check and updated print messages.

--- phish_detector.py
+++ phish_detector.py
@@ -1,5 +1,18 @@
-import os
-os.system("clear")
-os.system("git pull")
-print(" Welcome for 64 bit user")
-import detector
+import os, platform, time, sys
+print('\n\033[1;31m[\x1b[38;5;156m●\033[1;31m]\x1b[38;5;156m Checking For Update...')
+os.system('git pull')
+print('\x1b[38;5;208mNote \033[1;37m:\x1b[38;5;156m When setup is done, You see SetUp done msg')
+time.sleep(3)
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print('\033[1;31m[\x1b[38;5;156m●\033[1;31m]\x1b[38;5;156m You are 64 Bit user')
+    time.sleep(3)
+    os.system("clear") 
+    import detector
+elif bit == '32bit':
+    print('\033[1;31m[\x1b[38;5;156m●\033[1;31m]\x1b[38;5;156m You are 32 Bit user')
+    time.sleep(3)
+    os.system("clear")
+    
+else:
+    print("Sorry")
