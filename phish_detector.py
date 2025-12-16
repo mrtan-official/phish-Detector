2021d4aeef3d0043040281fdc4649cbad87a49bc
MR-TAN <135997970+mrtan-official@users.noreply.github.com>
2025-12-16 16:09:46 +0600

    Update phish_detector.py

--- phish_detector.py
+++ phish_detector.py
@@ -2,17 +2,13 @@ import os, platform, time, sys
 print('\n\033[1;31m[\x1b[38;5;156m●\033[1;31m]\x1b[38;5;156m Checking For Update...')
 os.system('git pull')
 print('Follow my facebook')
-xdg-open("https://www.facebook.com/MrT4N.Official")
+os.system("xdg-open https://www.facebook.com/MrT4N.Official")
 bit = platform.architecture()[0]
 if bit == '64bit':
     print('\033[1;31m[\x1b[38;5;156m●\033[1;31m]\x1b[38;5;156m You are 64 Bit user')
     time.sleep(3)
     os.system("clear") 
     import detector
-elif bit == '32bit':
-    print('\033[1;31m[\x1b[38;5;156m●\033[1;31m]\x1b[38;5;156mSorry You are 32 Bit user')
-    time.sleep(3)
-    os.system("clear")
     
 else:
     print("Sorry")
