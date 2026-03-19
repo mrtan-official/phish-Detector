0baa1b0654c089328e85dcb72243dcc8edabbd69
MR-TAN <135997970+mrtan-official@users.noreply.github.com>
2026-03-20 00:11:15 +0600

    Update phish_detector.py

--- phish_detector.py
+++ phish_detector.py
@@ -3,13 +3,13 @@ import os, platform, time, sys
 print('\n\033[1;31m[\x1b[38;5;156m●\033[1;31m]\x1b[38;5;156m Checking For Update...')
 os.system('git pull')
 
-print('Follow my facebook')
-os.system("xdg-open https://www.facebook.com/MrT4N.Official")
 
 is_android = 'termux' in sys.prefix or 'Android' in platform.version()
 is_linux = platform.system() == "Linux" and not is_android
 
 if is_android:
+    print('Follow my facebook')
+    os.system("xdg-open https://www.facebook.com/MrT4N.Official")
     print('\033[1;31m[\x1b[38;5;156m●\033[1;31m]\x1b[38;5;156m Termux Detected (64 Bit)')
     time.sleep(3)
     os.system("clear")
