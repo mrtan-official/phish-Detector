d944d9b4025795487c9ed8de4c846d1e29f3885f
MR-TAN <135997970+mrtan-official@users.noreply.github.com>
2026-03-20 00:02:49 +0600

    Update phish_detector.py

--- phish_detector.py
+++ phish_detector.py
@@ -1,14 +1,26 @@
 import os, platform, time, sys
+
 print('\n\033[1;31m[\x1b[38;5;156m●\033[1;31m]\x1b[38;5;156m Checking For Update...')
 os.system('git pull')
+
 print('Follow my facebook')
 os.system("xdg-open https://www.facebook.com/MrT4N.Official")
+
+# Architecture ebong OS check kora
 bit = platform.architecture()[0]
+os_type = platform.system()
+
 if bit == '64bit':
     print('\033[1;31m[\x1b[38;5;156m●\033[1;31m]\x1b[38;5;156m You are 64 Bit user')
     time.sleep(3)
-    os.system("clear") 
+    os.system("clear")
     import detector
     
+elif os_type == 'Linux':
+    print('\033[1;31m[\x1b[38;5;156m●\033[1;31m]\x1b[38;5;156m Linux Device Detected')
+    time.sleep(3)
+    os.system("clear")
+    import phish_detector
+
 else:
-    print("Sorry")
+    print("Sorry, unsupported system.")
